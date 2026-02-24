7c253ef1bcd0211c9657dfadcc6ce79ff2002ccd
Andrew Schtkan <texview@gmx.com>
2026-02-24 22:32:39 +0300

    Add serial lib for android

--- AlarmsTuner/Platforms/Android/MainActivity.cs
+++ AlarmsTuner/Platforms/Android/MainActivity.cs
@@ -1,10 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Hardware.Usb;
 using Android.OS;
 
 namespace AlarmsTuner
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter([UsbManager.ActionUsbDeviceAttached])]
     public class MainActivity : MauiAppCompatActivity
     {
     }
